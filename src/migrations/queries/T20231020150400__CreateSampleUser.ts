0c5cd21987ffa268aacd835396922d8771fdb549
cpenaforte <dannhl08@gmail.com>
2023-10-24 17:19:45 -0300

    feat: add production dockerfile and solve conflict inserting user

--- src/migrations/queries/T20231020150400__CreateSampleUser.ts
+++ src/migrations/queries/T20231020150400__CreateSampleUser.ts
@@ -2,7 +2,7 @@ import { PoolClient } from 'pg';
 
 /* eslint-disable no-unused-vars */
 module.exports = async (client: PoolClient): Promise<void> => {
-    await client.query('INSERT INTO users(user_password,full_name,email,sex,birthday) VALUES (\'123456\',\'Teste\',\'test@simpletasks.com.br\',\'male\',\'1999-08-26\')')
+    await client.query('INSERT INTO users(user_password,full_name,email,sex,birthday) VALUES (\'123456\',\'Teste\',\'test@simpletasks.com.br\',\'male\',\'1999-08-26\') ON CONFLICT DO UPDATE')
         .catch((error) => {
             if (error) {
                 console.log(error.message);
