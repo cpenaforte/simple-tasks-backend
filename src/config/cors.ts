69c685820ae1ec9177c2c42a7afb0819df083211
cpenaforte <dannhl08@gmail.com>
2023-10-25 12:23:48 -0300

    ci: allow cors for vercel app

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,9 @@ import { CorsOptions } from 'cors';
 
 import cors from 'cors';
 
-const whitelist: Array<string> = [ 'http://localhost:9000', 'http://localhost:3000' ];
+const whitelist: Array<string> = [
+    'http://localhost:9000', 'http://localhost:3000', 'https://simple-tasks-px8aac615-cpenaforte.vercel.app/',
+];
 
 const options: CorsOptions = {
     origin: whitelist,
