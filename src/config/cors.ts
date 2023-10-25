d4501f1bad45b8cdc7ce9dc37eea476a4f2115de
cpenaforte <dannhl08@gmail.com>
2023-10-25 12:26:50 -0300

    ci: use non hash frontend

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,7 +3,7 @@ import { CorsOptions } from 'cors';
 import cors from 'cors';
 
 const whitelist: Array<string> = [
-    'http://localhost:9000', 'http://localhost:3000', 'https://simple-tasks-px8aac615-cpenaforte.vercel.app/',
+    'http://localhost:9000', 'http://localhost:3000', 'simple-tasks-sandy.vercel.app',
 ];
 
 const options: CorsOptions = {
