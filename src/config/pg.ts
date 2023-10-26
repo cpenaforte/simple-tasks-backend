09ef244857a471a9278d24a679c1eb1a71863ef9
cpenaforte <dannhl08@gmail.com>
2023-10-26 10:22:47 -0300

    refactor: allow ssl in test

--- src/config/pg.ts
+++ src/config/pg.ts
@@ -6,7 +6,7 @@ const pool: Pool = new Pool({
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: parseInt(process.env.DB_PORT || '5432'),
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    ssl: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test' ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
